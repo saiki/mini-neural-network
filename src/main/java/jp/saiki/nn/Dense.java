d24bf719b2f5a72226047683c939d60da73e7627
saiki <akio.saiki.dev@gmail.com>
2019-02-08 14:13:04 +0900

    add weight and getter

--- src/main/java/jp/saiki/nn/Dense.java
+++ src/main/java/jp/saiki/nn/Dense.java
@@ -1,25 +1,44 @@
 package jp.saiki.nn;
 
+import java.util.Arrays;
+
 public class Dense implements Layer {
 
-    private double[] weight;
+    private double[][] weight;
 
     private double[] bias;
 
-    public Dense(double[] initWeight, double[] bias) {
+    public Dense(double[][] initWeight, double[] bias) {
         this.weight = initWeight;
         this.bias = bias;
     }
 
+    @Override
     public double[] forword(double[] input) {
-        double[] output = new double[input.length];
-        for (int i = 0; i < input.length; i++) {
-            output[i] = input[i] * weight[i] + bias[i];
+        double[] output = new double[this.weight[0].length];
+        Arrays.fill(output, 0d);
+        for (int i = 0; i < output.length; i++) {
+            for (int j = 0; j < input.length; j++) {
+                output[i] += input[j] * this.weight[j][i];
+            }
+            output[i] += this.bias[i];
         }
         return output;
     }
 
-    public void update(double[] newWeight) {
+    @Override
+    public double[][] getWeight() {
+        return this.weight;
+    }
+
+    @Override
+    public double[] getBias() {
+        return this.bias;
+    }
+
+    @Override
+    public void update(double[][] newWeight, double[] newBias) {
         this.weight = newWeight;
+        this.bias = newBias;
     }
 }
--- src/main/java/jp/saiki/nn/Layer.java
+++ src/main/java/jp/saiki/nn/Layer.java
@@ -3,6 +3,18 @@ package jp.saiki.nn;
 @FunctionalInterface
 public interface Layer {
 
+    public default double[][] getWeight() {
+        return null;
+    }
+
+    public default double[] getBias() {
+        return null;
+    }
+
+    public default void update(double[][] newWeight, double[] newBias) {
+        // nop
+    }
+
     public double[] forword(double[] input);
     
 }
